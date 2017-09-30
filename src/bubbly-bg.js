603590b00c5af8ea025b27996cd2bfc046c82d30
David <davidaase@hotmail.com>
2017-09-30 12:05:01 +0200

    add animations

--- src/bubbly-bg.js
+++ src/bubbly-bg.js
@@ -1,31 +1,59 @@
 window.bubbly = function (config) {
     let c = config || {};
-    let rand = () => Math.random();
+    let r = () => Math.random();
     const canvas = document.createElement("canvas");
+    document.body.appendChild(canvas);
     canvas.setAttribute("style", "position:fixed;z-index:-1;left:0;top:0;min-width:100vw;min-height:100vh;");
-    const context = canvas.getContext("2d");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    const context = canvas.getContext("2d");
     let gradient = context.createLinearGradient(0, 0, canvas.width, canvas.height);
     gradient.addColorStop(0, c.colorStart || "#25A6E1");
     gradient.addColorStop(1, c.colorStop || "#176EB5");
     context.fillStyle = gradient;
-    context.fillRect(0, 0, canvas.width, canvas.height); // gradient bg
-    context.globalCompositeOperation = c.compose || "lighter"; // add/blend color values
-    context.shadowColor = "#fff";
     let nrBubbles = c.bubbles || Math.floor((canvas.width + canvas.height) * 0.02);
-    context.shadowBlur = c.blur || (2 + rand() * 5);
+    let bubbles = [];
     for (let i = 0; i < nrBubbles; i++) {
-        context.beginPath();
-        context.arc(
-            rand() * canvas.width, // x position
-            rand() * canvas.height, // y position
-            4 + (rand() * canvas.width / 25),
-            0,
-            Math.PI * 2
-        );
-        context.fillStyle = (c.bubbleFunc || (() => `hsla(0, 0%, 100%, ${rand() * 0.1})`)).call();
-        context.fill();
+        bubbles.push({
+            fill: (c.bubbleFunc || (() => `hsla(0, 0%, 100%, ${r() * 0.1})`)).call(),
+            xPos: r() * canvas.width,
+            yPos: r() * canvas.height,
+            radius: 4 + (r() * canvas.width / 25),
+            start: 0,
+            angle: r() * Math.PI * 2,
+            velocity: 0.1 + r() * 0.5,
+            end: Math.PI * 2
+        });
     }
-    document.body.appendChild(canvas);
+    (function draw() {
+        if (c.animate) {
+            requestAnimationFrame(draw);
+        }
+        context.globalCompositeOperation = "source-over";
+        context.fillStyle = gradient;
+        context.fillRect(0, 0, canvas.width, canvas.height);
+        context.globalCompositeOperation = c.compose || "lighter";
+        bubbles.forEach(b => {
+            context.beginPath();
+            context.arc(b.xPos, b.yPos, b.radius, b.start, b.end);
+            context.fillStyle = b.fill;
+            context.fill();
+            context.shadowBlur = c.blur || (2 + r() * 5);
+            // update positions for next draw
+            b.xPos += Math.cos(b.angle) * b.velocity;
+            b.yPos += Math.sin(b.angle) * b.velocity;
+            if (b.xPos - b.radius > canvas.width) {
+                b.xPos = -b.radius;
+            }
+            if (b.xPos + b.radius < 0) {
+                b.xPos = canvas.width + b.radius;
+            }
+            if (b.yPos - b.radius > canvas.height) {
+                b.yPos = -b.radius;
+            }
+            if (b.yPos + b.radius < 0) {
+                b.yPos = canvas.height + b.radius;
+            }
+        });
+    })();
 };
