f870316287b4421f74c67333e88a95df80ba1872
David <davidaase@hotmail.com>
2017-10-01 14:49:03 +0200

    add width/height constants

--- src/bubbly-bg.js
+++ src/bubbly-bg.js
@@ -3,23 +3,23 @@ window.bubbly = function (config) {
     const r = () => Math.random();
     const canvas = document.createElement("canvas");
     canvas.setAttribute("style", "position:fixed;z-index:-1;left:0;top:0;min-width:100vw;min-height:100vh;");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    const width = canvas.width = window.innerWidth;
+    const height = canvas.height = window.innerHeight;
     document.body.appendChild(canvas); // todo(tipsy): consider allowing custom canvas
     const context = canvas.getContext("2d");
     context.shadowColor = c.shadowColor || "#fff";
     context.shadowBlur = c.blur || 4;
-    const gradient = context.createLinearGradient(0, 0, canvas.width, canvas.height);
+    const gradient = context.createLinearGradient(0, 0, width, height);
     gradient.addColorStop(0, c.colorStart || "#25A6E1");
     gradient.addColorStop(1, c.colorStop || "#176EB5");
-    const nrBubbles = c.bubbles || Math.floor((canvas.width + canvas.height) * 0.02);
+    const nrBubbles = c.bubbles || Math.floor((width + height) * 0.02);
     const bubbles = [];
     for (let i = 0; i < nrBubbles; i++) {
         bubbles.push({
             f: (c.bubbleFunc || (() => `hsla(0, 0%, 100%, ${r() * 0.1})`)).call(), // fillStyle
-            x: r() * canvas.width, // x-position
-            y: r() * canvas.height, // y-position
-            r: 4 + (r() * canvas.width / 25), // radius
+            x: r() * width, // x-position
+            y: r() * height, // y-position
+            r: 4 + (r() * width / 25), // radius
             a: r() * Math.PI * 2, // angle
             v: 0.1 + r() * 0.5 // velocity
         });
@@ -30,7 +30,7 @@ window.bubbly = function (config) {
         }
         context.globalCompositeOperation = "source-over";
         context.fillStyle = gradient;
-        context.fillRect(0, 0, canvas.width, canvas.height);
+        context.fillRect(0, 0, width, height);
         context.globalCompositeOperation = c.compose || "lighter";
         bubbles.forEach(bubble => {
             context.beginPath();
@@ -40,17 +40,17 @@ window.bubbly = function (config) {
             // update positions for next draw
             bubble.x += Math.cos(bubble.a) * bubble.v;
             bubble.y += Math.sin(bubble.a) * bubble.v;
-            if (bubble.x - bubble.r > canvas.width) {
+            if (bubble.x - bubble.r > width) {
                 bubble.x = -bubble.r;
             }
             if (bubble.x + bubble.r < 0) {
-                bubble.x = canvas.width + bubble.r;
+                bubble.x = width + bubble.r;
             }
-            if (bubble.y - bubble.r > canvas.height) {
+            if (bubble.y - bubble.r > height) {
                 bubble.y = -bubble.r;
             }
             if (bubble.y + bubble.r < 0) {
-                bubble.y = canvas.height + bubble.r;
+                bubble.y = height + bubble.r;
             }
         });
     })();
