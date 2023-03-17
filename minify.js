9670ea55678f362f7d5b78b8b689a933158f1f77
tipsy <davidaase@hotmail.com>
2023-03-17 18:21:59 +0100

    replace gulp

---
+++ minify.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const Terser = require("terser");
+
+(async () => {
+    let minified = (await Terser.minify(fs.readFileSync("src/bubbly-bg.js", "utf8"))).code;
+    fs.writeFileSync("dist/bubbly-bg.js", minified, "utf8");
+})();
