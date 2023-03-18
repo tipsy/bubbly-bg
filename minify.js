c9c8d78c9d694076669861dfd7b00c13e52144a5
tipsy <davidaase@hotmail.com>
2023-03-18 10:49:57 +0100

    include docs in minify

--- minify.js
+++ minify.js
@@ -4,4 +4,5 @@ const Terser = require("terser");
 (async () => {
     let minified = (await Terser.minify(fs.readFileSync("src/bubbly-bg.js", "utf8"))).code;
     fs.writeFileSync("dist/bubbly-bg.js", minified, "utf8");
+    fs.writeFileSync("docs/bubbly-bg.js", minified, "utf8");
 })();
